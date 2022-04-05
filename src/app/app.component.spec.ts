dfb8becc836e920f9f921b907fade3d851cb1e91
shehabAbouelhassan <55254824+shehabAbouelhassan@users.noreply.github.com>
2022-04-05 22:22:26 +0200

    final commit of FrontEnd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'assignmentClientSide'`, () => {
+  it(`should have as title 'angular-book-seller'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('assignmentClientSide');
+    expect(app.title).toEqual('angular-book-seller');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('assignmentClientSide app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-book-seller app is running!');
   });
 });
